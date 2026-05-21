a6f39246a3bf82974363981c0150f2332d0b05a9
sarah <sarahmudor@gmail.com>
2026-05-21 22:59:51 +0200

    feat: add navigation + rename menu point pages

--- my-nextui-app/app/layout.tsx
+++ my-nextui-app/app/layout.tsx
@@ -1,13 +1,11 @@
 import "@/styles/globals.css";
-import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
+import { Metadata, Viewport } from "next";
 
 import { Providers } from "./providers";
 
-import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import { siteConfig } from "@/config/site";
 
 export const metadata: Metadata = {
   title: {
@@ -35,23 +33,24 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head>
-<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-</head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={clsx(
           "min-h-screen font-sans antialiased",
           fontSans.variable,
-        )}
-      >
+        )}>
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-          <div className="relative flex flex-col h-screen">
+          <div className="relative flex flex-col h-screen bg-gradient-to-br from-amber-50 to-fuchsia-300">
             <main className="light container mx-auto pt-16 px-6 flex-grow">
               {children}
             </main>
             <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=HAGT_RgyE-71zT5o&amp;list=PLd4YajkcrhkFV30j6bcCQxptsOj0s_FEO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-              Potato
-              </div>
+              {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=HAGT_RgyE-71zT5o&amp;list=PLd4YajkcrhkFV30j6bcCQxptsOj0s_FEO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
+            </div>
           </div>
         </Providers>
       </body>
--- my-nextui-app/app/page.tsx
+++ my-nextui-app/app/page.tsx
@@ -1,83 +1,90 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import { Chip } from "@nextui-org/chip";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { Chip } from "@nextui-org/chip";
 
-import { Image } from "@nextui-org/image";
 import { Divider } from "@nextui-org/divider";
-import { useTheme } from "next-themes";
+import { Image } from "@nextui-org/image";
+import { Navbar } from "@/components/navbar";
 
-
-
-
-const chipContents = ['emojis ✨', 'sustainability 🌱', 'art', 'figma', 'يمن']
+const chipContents = ["emojis ✨", "sustainability 🌱", "art", "figma", "يمن"];
 const cardContents = [
-  {title: 'work', content: 'Software Engineer / UX Engineer', icon: 'keyboard'},
-  {title: 'studies', content: 'Masters in Media Computer Science', icon: 'school'},
-  {title: 'private stuff', contentChips: ['Drawing', 'Choir', 'Languages'], icon: 'stock_media'}]
+  {
+    title: "work",
+    content: "Software Engineer / UX Engineer",
+    icon: "keyboard",
+  },
+  {
+    title: "studies",
+    content: "Masters in Media Computer Science",
+    icon: "school",
+  },
+  {
+    title: "private stuff",
+    contentChips: ["Drawing", "Choir", "Languages"],
+    icon: "stock_media",
+  },
+];
 
+// export const ThemeSwitcher = () => {
+//   const { theme, setTheme } = useTheme()
 
-  // export const ThemeSwitcher = () => {
-  //   const { theme, setTheme } = useTheme()
-  
-  //   return (
-  //     <div>
-  //       The current theme is: {theme}
-  //       <button onClick={() => setTheme('light')}>Light Mode</button>
-  //       <button onClick={() => setTheme('dark')}>Dark Mode</button>
-  //     </div>
-  //   )
-  // };
+//   return (
+//     <div>
+//       The current theme is: {theme}
+//       <button onClick={() => setTheme('light')}>Light Mode</button>
+//       <button onClick={() => setTheme('dark')}>Dark Mode</button>
+//     </div>
+//   )
+// };
 
-  
 export default function Home() {
-  // const { theme, setTheme } = useTheme(); 
+  // const { theme, setTheme } = useTheme();
 
   return (
-    <div className="flex flex-col h-full gap-6">
-      <main>
-        <div className="flex flex-row gap-10">
-        <Image
-      width={150}
-      alt="Moon Picture"
-      src="/image.png"
-    />
-        <div className="flex flex-col w-full gap-10">
-         <span>Sarah Alaghbari</span>
-         <div className="flex gap-4">
-        { chipContents.map(chipContent => <Chip variant="shadow" 
-      className="pretty-chips"><b>#{chipContent}</b></Chip>)}
+    <div className="flex flex-row gap-2 ">
+      <Navbar />
+      <div className="flex flex-col gap-6 h-full">
+        <Image width={150} alt="Moon Picture" src="/sun.png" />
+        <span className="text-4xl font-bold">Hi, I'm Sarah</span>
+        <span className="text-2xl text-gray-500">
+          I'm a software engineer with a passion for user experience and design.
+        </span>
+        <div className="flex gap-4">
+          {chipContents.map((chipContent) => (
+            <Chip variant="shadow" className="pretty-chips">
+              <b>#{chipContent}</b>
+            </Chip>
+          ))}
+        </div>
+        <Divider orientation="horizontal" />
+        <div className="gap-3 grid grid-cols-3 lg:grid-cols-3 w-full">
+          {cardContents.map((item, index) => (
+            <Card
+              shadow="sm"
+              key={index}
+              className="p-4 skillCard flex justify-center"
+              isPressable>
+              <CardHeader className="flex flex-col justify-center gap-4">
+                <span className="material-symbols-outlined item-icon">
+                  {item.icon}
+                </span>
+                <h2 className="text-2xl font-['Gupter'] tracking-widest text-indigo-400">
+                  {item.title}
+                </h2>
+              </CardHeader>
+              <CardBody className="overflow-visible">
+                <div className="flex gap-4 justify-center">
+                  {item.content}
+                  {item.contentChips?.map((chip) => (
+                    <Chip variant="shadow" className="pretty-chips">
+                      {chip}
+                    </Chip>
+                  ))}
+                </div>
+              </CardBody>
+            </Card>
+          ))}
+        </div>
       </div>
-      </div>
-      </div>
-         </main>
-         <Divider orientation="horizontal" />
-         <Divider orientation="horizontal" />
-
-<div className="gap-3 grid grid-cols-3 lg:grid-cols-3 w-full">
-{cardContents.map((item, index) => (
-  <Card shadow="sm" key={index} className="p-4 skillCard flex justify-center" isPressable>
-    <CardHeader className="flex flex-col justify-center gap-4">
-      <span className="material-symbols-outlined item-icon">{item.icon}</span>
-      <h2 className="text-2xl font-['Gupter'] tracking-widest text-indigo-400">{item.title}</h2>
-      </CardHeader>
-    <CardBody className="overflow-visible">
-      <div className="flex gap-4 justify-center">
-    {item.content}
-    {item.contentChips?.map((chip) => (
-      <Chip variant="shadow" 
-      className="pretty-chips">{chip}</Chip>
-    ))}</div>
-    </CardBody>
-  </Card>
-))}
-</div>
-</div>
+    </div>
   );
 }
